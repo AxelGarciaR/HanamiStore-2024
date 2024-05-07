13211c68c7436877acb7ba226093db1bc8d41ad4
Jhirdand <133984359+Jhirdand@users.noreply.github.com>
2024-05-07 09:13:18 -0600

    modal

--- Controllers/admin/productos.js
+++ Controllers/admin/productos.js
@@ -1,38 +1,37 @@
 let contador = 0;
 
-    function agregarProducto() {
-      contador++;
-      const container = document.querySelector('.product-card-container');
+function agregarProducto() {
+  contador++;
+  const container = document.querySelector('.product-card-container');
 
-      const card = document.createElement('div');
-      card.className = 'card selectable-card';
-      card.dataset.id = contador;
-      card.innerHTML = `
-        <img src="../../Resources/images/loreal.png" class="card-img-top" alt="Producto ${contador}" width="100" height="200">
-        <div class="card-body">
-          <h5 class="card-title">Producto ${contador}</h5>
-          <p class="card-text">Descripción del producto ${contador}.</p>
-          <div class="card-actions">
-            <button class="btn btn-info" onclick="mostrarModal(${contador})">Información</button>
-            <button class="btn btn-danger" onclick="eliminarProducto(event)">Eliminar</button>
-          </div>
-        </div>
-      `;
+  const card = document.createElement('div');
+  card.className = 'card selectable-card';
+  card.dataset.id = contador;
+  card.innerHTML = `
+    <img src="../../Resources/images/loreal.png" class="card-img-top" alt="Producto 1" width="100" height="200">
+    <div class="card-body">
+      <h5 class="card-title">Producto 1</h5>
+      <p class="card-text">Descripción del producto 1.</p>
+      <p class="card-text">precio 1.</p>
+      <p class="card-text">cantidad 1.</p>
+      <div class="card-actions">
+        <button class="btn btn-info rosado-btn" onclick="mostrarModal(1)">Editar</button>
+        <button class="btn btn-danger" onclick="confirmarEliminar(event)">Eliminar</button>
+      </div>
+    </div>
+  </div>`;
 
-      container.appendChild(card);
-    }
+  container.appendChild(card);
+}
 
-    function eliminarProducto(event) {
-      const card = event.target.closest('.selectable-card');
-      card.remove();
-    }
+function confirmarEliminar(event) {
+  const confirmed = confirm("¿Estás seguro que quieres eliminar este producto?");
+  if (confirmed) {
+    eliminarProducto(event);
+  }
+}
 
-    function mostrarModal(tarjetaId) {
-      const modal = document.getElementById('modal' + tarjetaId);
-      modal.style.display = 'block';
-    }
-
-    function cerrarModal(tarjetaId) {
-      const modal = document.getElementById('modal' + tarjetaId);
-      modal.style.display = 'none';
-    }
+function eliminarProducto(event) {
+  const card = event.target.closest('.selectable-card');
+  card.remove();
+}
