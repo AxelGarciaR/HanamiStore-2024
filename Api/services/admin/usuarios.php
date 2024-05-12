4a3d479460c8a2045abfdfecebff769eca307c49
AxelGarciaR <20220127@ricaldone.edu.sv>
2024-05-12 13:58:28 -0600

    correccion de usuario service

--- Api/services/admin/usuarios.php
+++ Api/services/admin/usuarios.php
@@ -11,7 +11,8 @@ if (isset($_GET['action'])) {
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'message' => null, 'dataset' => null, 'error' => null, 'exception' => null, 'fileStatus' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    if (isset($_SESSION['idUsuario'])) {
+    if (isset($_SESSION['idAdministrador'])) {
+        $result['session'] = 1;
         // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
         switch ($_GET['action']) {
             case 'searchRows':
@@ -35,11 +36,11 @@ if (isset($_GET['action'])) {
                     $result['error'] = $usuario->getDataError();
                 } elseif ($usuario->createRow()) {
                     $result['status'] = 1;
-                    $result['message'] = 'Producto creado correctamente';
+                    $result['message'] = 'usuario creado correctamente';
                     // Se asigna el estado del archivo después de insertar.
                     $result['fileStatus'] = Validator::saveFile($_FILES['imagenProducto'], $usuario::RUTA_IMAGEN);
                 } else {
-                    $result['error'] = 'Ocurrió un problema al crear el producto';
+                    $result['error'] = 'Ocurrio un problema con ingresar un admin';
                 }
                 break;
             case 'readAll':
@@ -70,11 +71,11 @@ if (isset($_GET['action'])) {
                     $result['error'] = $usuario->getDataError();
                 } elseif ($usuario->updateRow()) {
                     $result['status'] = 1;
-                    $result['message'] = 'Producto modificado correctamente';
+                    $result['message'] = 'Administrador actualizado';
                     // Se asigna el estado del archivo después de actualizar.
                     $result['fileStatus'] = Validator::changeFile($_FILES['imagenUsuario'], $usuario::RUTA_IMAGEN, $usuario->getFilename());
                 } else {
-                    $result['error'] = 'Ocurrió un problema al modificar el producto';
+                    $result['error'] = 'Ocurrió un problema al modificar un admin';
                 }
                 break;
             case 'deleteRow':
@@ -95,6 +96,14 @@ if (isset($_GET['action'])) {
                     $result['message'] = 'Sesión eliminada correctamente';
                 } else {
                     $result['error'] = 'Ocurrió un problema al cerrar la sesión';
+                }
+                break;
+            case 'getUser':
+                if (isset($_SESSION['aliasAdmin'])) {
+                    $result['status'] = 1;
+                    $result['username'] = $_SESSION['aliasAdmin'];
+                } else {
+                    $result['error'] = 'Alias de administrador no encontrado';
                 }
                 break;
             case 'readProfile':
@@ -171,9 +180,12 @@ if (isset($_GET['action'])) {
                 break;
             case 'logIn':
                 $_POST = Validator::validateForm($_POST);
-                if ($usuario->checkUser($_POST['correo'], $_POST['clave'])) {
+                if ($usuario->checkUser($_POST['correoLogin'], $_POST['claveLogin'])) {
                     $result['status'] = 1;
-                    $result['message'] = 'Autenticación correcta';
+                    $result['idadmin'] = $_SESSION['idAdministrador'];
+                    $result['message'] = 
+                    'Autenticación correcta';
+
                 } else {
                     $result['error'] = 'Credenciales incorrectas';
                 }
@@ -181,13 +193,13 @@ if (isset($_GET['action'])) {
             default:
                 $result['error'] = 'Acción no disponible fuera de la sesión';
         }
-        // Se obtiene la excepción del servidor de base de datos por si ocurrió un problema.
-        $result['exception'] = Database::getException();
-        // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
-        header('Content-type: application/json; charset=utf-8');
-        // Se imprime el resultado en formato JSON y se retorna al controlador.
-        print(json_encode($result));
     }
+     // Se obtiene la excepción del servidor de base de datos por si ocurrió un problema.
+     $result['exception'] = Database::getException();
+     // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+     header('Content-type: application/json; charset=utf-8');
+     // Se imprime el resultado en formato JSON y se retorna al controlador.
+     print(json_encode($result));
 } else {
     print(json_encode('Recurso no disponible'));
 }
