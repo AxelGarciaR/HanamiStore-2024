917d326e459569506168bc52b64790690999267b
AxelGarciaR <20220127@ricaldone.edu.sv>
2024-05-13 21:05:54 -0600

    a

--- Controllers/admin/index.js
+++ Controllers/admin/index.js
@@ -1,20 +1,20 @@
-
 // Constante para establecer el formulario de inicio de sesión.
 const LOGIN_FORM = document.getElementById('loginForm');
 
 // Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
     // Petición para consultar los usuarios registrados.
-    const DATA = await fetchData(USER_API, 'readProfile');
+    const DATA = await fetchData(USER_API, 'readUsers');
     // Se comprueba si existe una sesión, de lo contrario se sigue con el flujo normal.
     if (DATA.session) {
         // Se direcciona a la página web de bienvenida.
-        location.href = 'dashboard.html';
+        location.href = 'Graficas.html';
     } else if (DATA.status) {
-    } else {
+        // Se muestra el formulario para iniciar sesión.
+        LOGIN_FORM.classList.remove('d-none');
+        sweetAlert(4, DATA.message, true);
     }
 });
-
 
 // Método del evento para cuando se envía el formulario de inicio de sesión.
 LOGIN_FORM.addEventListener('submit', async (event) => {
@@ -27,7 +27,6 @@ LOGIN_FORM.addEventListener('submit', async (event) => {
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (DATA.status) {
         sweetAlert(1, DATA.message, true, 'Graficas.html');
-        console.log('peneeee');
     } else {
         sweetAlert(2, DATA.error, false);
     }
