b85413e3f126ea367874a51c836f54007984bc90
stanlyy <20220127@ricaldone.edu.sv>
2024-07-24 07:52:02 -0600

    Cambios por el choque de commits

--- Api/models/handler/productos_handler.php
+++ Api/models/handler/productos_handler.php
@@ -1,9 +1,9 @@
 <?php
 // Se incluye la clase para trabajar con la base de datos.
 require_once ('../../helpers/database.php');
-
+ 
 //Esta clase es para manejar el comportamiento de los datos de la tabla Usuarios
-
+ 
 class ProductosHandler
 {
     //Declaracion de atributos para el manejo de los datos de la tabla en la base de datos
@@ -16,12 +16,12 @@ class ProductosHandler
     protected $idSubcategoria = null;
     protected $descuento = null;
     protected $marca = null;
-
+ 
     // Constante para establecer la ruta de las imagenes
     const RUTA_IMAGEN = '../../images/productos/';
-
+ 
     //Aqui empiesan las funciones SCRUD
-
+ 
     //Esta funcion es para buscar productos
     public function searchRows()
     {
@@ -32,7 +32,7 @@ class ProductosHandler
         $params = array($value, $value);
         return Database::getRows($sql, $params);
     }
-
+ 
     //Esta funcion es para crear productos
     public function createRow()
     {
@@ -41,7 +41,7 @@ class ProductosHandler
         $params = array($this->nombreProducto, $this->descripcionProducto, $this->precioProducto, $this->imagenPrincipal, $this->cantidadProducto, $this->idSubcategoria, $this->descuento, $this->marca);
         return Database::executeRow($sql, $params);
     }
-
+ 
     //Esta funcion es para leer todos los productos
     public function readAll()
     {
@@ -50,17 +50,39 @@ class ProductosHandler
                 ORDER BY Nombre_Producto';
         return Database::getRows($sql);
     }
-
-    
-    public function readByMarca() {
-        $sql = 'SELECT p.Nombre_Producto, p.precio_producto, p.CantidadP, s.nombre AS nombre_subcategoria
-                FROM productos p
-                JOIN sub_categorias s ON p.id_subcategoria = s.id_SubCategoria
-                WHERE p.id_Marca = ?';
-        $params = array($this->marca);
-        return Database::getRows($sql, $params);
+ 
+    public function cantidadProductosCategoria()
+    {
+        $sql = 'SELECT nombre, COUNT(id_producto) CantidadP
+                FROM productos
+                INNER JOIN sub_categorias USING(id_SubCategoria)
+                GROUP BY nombre ORDER BY CantidadP DESC LIMIT 5';
+        return Database::getRows($sql);
     }
-
+ 
+    public function porcentajeProductosCategoria()
+    {
+        $sql = 'SELECT nombre, ROUND((COUNT(id_producto) * 100.0 / (SELECT COUNT(id_producto) FROM productos)), 2) porcentaje
+                FROM productos
+                INNER JOIN sub_categorias USING(id_SubCategoria)
+                GROUP BY nombre ORDER BY porcentaje DESC';
+        return Database::getRows($sql);
+    }
+ 
+    public function productosMasVendidos()
+    {
+        $sql = 'SELECT Nombre_Producto, SUM(cantidad) AS total_vendido
+            FROM detalleordenes
+            INNER JOIN productos ON detalleordenes.id_producto = productos.id_producto
+            GROUP BY Nombre_Producto
+            ORDER BY total_vendido DESC
+            LIMIT 5';  // Agrega LIMIT 5 para obtener solo los 5 productos más vendidos
+        return Database::getRows($sql);
+    }
+ 
+ 
+ 
+ 
     //Esta funcion es para hacer select de los productos de mayor id a menor para ver cuales son los mas nuevos
     public function NewProduct()
     {
@@ -70,7 +92,7 @@ class ProductosHandler
                 LIMIT 10';
         return Database::getRows($sql);
     }
-
+ 
     //Esta funcion es para leer un producto en especifico
     public function readOne()
     {
@@ -80,13 +102,36 @@ class ProductosHandler
         $params = array($this->idProducto);
         return Database::getRow($sql, $params);
     }
-
+ 
+    public function ventasPorMes()
+    {
+        $sql = 'SELECT DATE_FORMAT(m.fecha, \'%M\') AS mes, COALESCE(SUM(dp.cantidad), 0) AS cantidad_ventas
+            FROM (
+            SELECT DATE_ADD(\'2023-01-01\', INTERVAL (a.n + 10 * b.n) MONTH) AS fecha
+            FROM (
+            SELECT 0 AS n UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL
+            SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9
+            ) AS a
+            CROSS JOIN (
+                            SELECT 0 AS n UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL
+                            SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9
+                        ) AS b
+                    ) AS m
+                    LEFT JOIN ordenes p ON MONTH(p.Fecha_Orden) = MONTH(m.fecha) AND YEAR(p.Fecha_Orden) = YEAR(m.fecha)
+                    LEFT JOIN detalleordenes dp ON p.id_Orden = dp.id_Orden
+                    GROUP BY DATE_FORMAT(m.fecha, \'%M\')
+                    ORDER BY MONTH(m.fecha)';
+       
+            return Database::getRows($sql);
+        }
+ 
+ 
     //Esta funcion es para actualizar un producto
     public function updateRow()
     {
         $sql = 'UPDATE productos
                 SET Nombre_Producto = ?,
-                descripcion_producto = ?, 
+                descripcion_producto = ?,
                 precio_producto = ?,
                 imagen_principal = ?,
                 CantidadP = ?,
@@ -97,7 +142,7 @@ class ProductosHandler
         $params = array($this->nombreProducto, $this->descripcionProducto, $this->precioProducto, $this->imagenPrincipal, $this->cantidadProducto, $this->idSubcategoria, $this->descuento, $this->marca, $this->idProducto);
         return Database::executeRow($sql, $params);
     }
-
+ 
     //Esta funcion es para eliminar un producto
     public function deleteRow()
     {
@@ -106,7 +151,7 @@ class ProductosHandler
         $params = array($this->idProducto);
         return Database::executeRow($sql, $params);
     }
-
+ 
     // funcion para ver la imagen en la tabla del producto correspondiente
     public function readFilename()
     {
@@ -117,4 +162,13 @@ class ProductosHandler
         return Database::getRow($sql, $params);
     }
 
+    public function readByMarca() {
+        $sql = 'SELECT p.Nombre_Producto, p.precio_producto, p.CantidadP, s.nombre AS nombre_subcategoria
+                FROM productos p
+                JOIN sub_categorias s ON p.id_subcategoria = s.id_SubCategoria
+                WHERE p.id_Marca = ?';
+        $params = array($this->marca);
+        return Database::getRows($sql, $params);
+    }
+ 
 }
--- Controllers/admin/graficas.js
+++ Controllers/admin/graficas.js
@@ -15,10 +15,15 @@ document.addEventListener('DOMContentLoaded', () => {
     } else if (HOUR <= 23) {
         greeting = 'Buenas noches';
     }
+    
     // Llamada a la funciones que generan los gráficos en la página web.
     graficoBarrasCategorias();
     graficoPastelCategorias();
+    graficoProductosVendidos();
+    graficoVentasMes();
+    graficoEdadesClientes(); // Corrección aquí
 });
+
 
 /*
 *   Función asíncrona para mostrar un gráfico de barras con la cantidad de productos por categoría.
@@ -31,22 +36,22 @@ const graficoBarrasCategorias = async () => {
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
     if (DATA.status) {
         // Se declaran los arreglos para guardar los datos a graficar.
-        let categorias = [];
+        let Subcategorias = [];
         let cantidades = [];
         // Se recorre el conjunto de registros fila por fila a través del objeto row.
         DATA.dataset.forEach(row => {
             // Se agregan los datos a los arreglos.
-            categorias.push(row.nombre_categoria);
-            cantidades.push(row.cantidad);
+           Subcategorias.push(row.nombre);
+            cantidades.push(row.CantidadP);
         });
         // Llamada a la función para generar y mostrar un gráfico de barras. Se encuentra en el archivo components.js
-        barGraph('chart1', categorias, cantidades, 'Cantidad de productos', 'Cantidad de productos por categoría');
+        barGraph('chart1', Subcategorias, cantidades, 'Cantidad de productos', 'Cantidad de productos por Sub categoría');
     } else {
         document.getElementById('chart1').remove();
         console.log(DATA.error);
     }
 }
-
+ 
 /*
 *   Función asíncrona para mostrar un gráfico de pastel con el porcentaje de productos por categoría.
 *   Parámetros: ninguno.
@@ -63,7 +68,7 @@ const graficoPastelCategorias = async () => {
         // Se recorre el conjunto de registros fila por fila a través del objeto row.
         DATA.dataset.forEach(row => {
             // Se agregan los datos a los arreglos.
-            categorias.push(row.nombre_categoria);
+            categorias.push(row.nombre);
             porcentajes.push(row.porcentaje);
         });
         // Llamada a la función para generar y mostrar un gráfico de pastel. Se encuentra en el archivo components.js
@@ -73,6 +78,110 @@ const graficoPastelCategorias = async () => {
         console.log(DATA.error);
     }
 }
+ 
+const graficoProductosVendidos = async () => {
+    const DATA = await fetchData(PRODUCTO_API, 'productosMasVendidos');
+    if (DATA.status) {
+        let productos = [];
+        let cantidades = [];
+        DATA.dataset.forEach(row => {
+            productos.push(row.Nombre_Producto);
+            cantidades.push(row.total_vendido);
+        });
+        renderChartPolar(document.getElementById('chart3').getContext('2d'), productos, cantidades, 'Productos más Vendidos');
+    } else {
+        document.getElementById('chart3').remove();
+        console.log(DATA.error);
+    }
+}
+ 
+const renderChartPolar = (context, labels, data, title) => {
+    const randomColors = generateRandomColors(labels.length); // Generar colores aleatorios
+ 
+    new Chart(context, {
+        type: 'polarArea',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: title,
+                data: data,
+                backgroundColor: randomColors
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+};
+ 
+// Función para generar colores aleatorios en formato RGB
+const generateRandomColors = (numColors) => {
+    const colors = [];
+    for (let i = 0; i < numColors; i++) {
+        const r = Math.floor(Math.random() * 256); // Valor aleatorio para rojo (0-255)
+        const g = Math.floor(Math.random() * 256); // Valor aleatorio para verde (0-255)
+        const b = Math.floor(Math.random() * 256); // Valor aleatorio para azul (0-255)
+        colors.push(`rgb(${r}, ${g}, ${b})`); // Agregar color en formato RGB al arreglo
+    }
+    return colors;
+};
+ 
+const graficoVentasMes = async () => {
+    const DATA = await fetchData(PRODUCTO_API, 'ventasPorMes');
+    if (DATA.status) {
+        let meses = [];
+        let ventas = [];
+        DATA.dataset.forEach(row => {
+            meses.push(row.mes);
+            ventas.push(row.cantidad_ventas);
+        });
+        renderChart(document.getElementById('chartVentasMes').getContext('2d'), 'line', meses, ventas, 'Ventas por Mes');
+    } else {
+        document.getElementById('chartVentasMes').remove();
+        console.log(DATA.error);
+    }
+}
+ 
+const graficoEdadesClientes = async () => {
+    const DATA = await fetchData(CLIENTE_API, 'edadesClientes'); // Usar CLIENTE_API en lugar de PRODUCTO_API
+    if (DATA.status) {
+        let rangos = [];
+        let cantidades = [];
+        DATA.dataset.forEach(row => {
+            rangos.push(row.rango);
+            cantidades.push(row.cantidad);
+        });
+        renderChart(document.getElementById('chartEdadesClientes').getContext('2d'), 'bar', rangos, cantidades, 'Edades de Clientes');
+    } else {
+        document.getElementById('chartEdadesClientes').remove();
+        console.log(DATA.error);
+    }
+}
+ 
+// Función para renderizar los gráficos
+const renderChart = (context, type, labels, data, title) => {
+    new Chart(context, {
+        type: type,
+        data: {
+            labels: labels,
+            datasets: [{
+                label: title,
+                data: data,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+};
 
 /****LLAMADA PARA ABRIR LAS GRAFICAS****/
 
