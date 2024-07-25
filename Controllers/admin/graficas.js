d53da73bce5877e8b5675c60020d1f4b05eb6f7f
Jhirdand <133984359+Jhirdand@users.noreply.github.com>
2024-07-25 13:41:46 -0600

    graficas

--- Controllers/admin/graficas.js
+++ Controllers/admin/graficas.js
@@ -215,3 +215,16 @@ const openReportEmpleados = () => {
     // Se abre el reporte en una nueva pestaña.
     window.open(PATH.href);
 }
+const openReportProductosCategoria = () => {
+    // Se declara una constante tipo objeto con la ruta específica del reporte en el servidor.
+    const PATH = new URL(`${SERVER_URL}reports/admin/productos_categoria_report.php`);
+    // Se abre el reporte en una nueva pestaña.
+    window.open(PATH.href);
+}
+
+const openReportOrdenes = () => {
+    // Se declara una constante tipo objeto con la ruta específica del reporte en el servidor.
+    const PATH = new URL(`${SERVER_URL}reports/admin/ordenes_report.php`);
+    // Se abre el reporte en una nueva pestaña.
+    window.open(PATH.href);
+}
